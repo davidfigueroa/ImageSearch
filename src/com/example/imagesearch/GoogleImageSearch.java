94bce641ee4130ddae312fc90bac48cc4e709c01
David <davidfig@yahoo-inc.com>
2014-06-15 07:47:48 -0700

    changed to JsonHtppResponseHandler

--- src/com/example/imagesearch/GoogleImageSearch.java
+++ src/com/example/imagesearch/GoogleImageSearch.java
@@ -1,11 +1,10 @@
 package com.example.imagesearch;
 
-import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class GoogleImageSearch {
@@ -18,31 +17,33 @@ public class GoogleImageSearch {
 	      params.add("rsz", "8");
 	      params.add("start", String.valueOf(start));
 	      params.add("q", queryString);
-	      client.get(BASE_URL, params, new AsyncHttpResponseHandler() {
-	    	@Override
-	    	public void onSuccess(int httpCode, Header[] arg1, byte[] arg2) {
+	      client.get(BASE_URL, params, new JsonHttpResponseHandler() {
+	    	  public void onSuccess(JSONObject json) {
 	    		try {
-					String responseContent = new String(arg2, "UTF-8");
-		    		if (httpCode == 200) {
-		    			JSONObject json = new JSONObject(responseContent);
-		    			JSONArray results = json.getJSONObject("responseData").getJSONArray("results");
-		    			ImageInfo[] ret = new ImageInfo[results.length()];
-		    			for (int i = 0; i < results.length(); i++) {
-		    				JSONObject o = (JSONObject) results.get(i);
-		    				ImageInfo im = new ImageInfo(o.getString("url"), o.getString("tbUrl"));
-		    				ret[i] = im;
-		    			}
-		    			responseHandler.onSuccess(ret);
-		    		} else {
-		    			responseHandler.onError(new Throwable(responseContent));
-		    		}
+	    			JSONArray results = json.getJSONObject("responseData").getJSONArray("results");
+	    			ImageInfo[] ret = new ImageInfo[results.length()];
+	    			for (int i = 0; i < results.length(); i++) {
+	    				JSONObject o = (JSONObject) results.get(i);
+	    				ImageInfo im = new ImageInfo(o.getString("url"), o.getString("tbUrl"));
+	    				ret[i] = im;
+	    			}
+	    			responseHandler.onSuccess(ret);
 				} catch (Exception e) {
 					responseHandler.onError(e);
 				}
-	    	}
-	    	@Override
-	    	public void onFailure(int arg0, Header[] arg1, byte[] arg2,	Throwable arg3) {
-	    	}
+	    	  };
+	    	  public void onFailure(Throwable e, JSONObject json) {
+				try {
+					if (e != null) {
+						responseHandler.onError(e);
+					} else {
+						String errorMessage = json.getString("responseDetails");
+						responseHandler.onError(new Exception(errorMessage));
+					}
+				} catch (Exception e2) {
+					responseHandler.onError(e2);
+				}
+	    	  };
 	      });
 	 }
 	
