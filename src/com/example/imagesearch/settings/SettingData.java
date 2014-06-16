67e0dedfdbc15c7a4bb78f2fdd455c42d6f489b5
David <davidfig@yahoo-inc.com>
2014-06-16 06:57:25 -0700

    setting spinners

--- src/com/example/imagesearch/settings/SettingData.java
+++ src/com/example/imagesearch/settings/SettingData.java
@@ -9,23 +9,41 @@ public class SettingData implements Serializable {
 	private ImageType imageType;
 	private String site;	
 	
+	public ImageSize getImageSize() {
+		return imageSize;
+	}
+
+	public void setImageSize(ImageSize imageSize) {
+		this.imageSize = imageSize;
+	}
+
+	public ColorFilter getColorFilter() {
+		return colorFilter;
+	}
+
+	public void setColorFilter(ColorFilter colorFilter) {
+		this.colorFilter = colorFilter;
+	}
+
+	public ImageType getImageType() {
+		return imageType;
+	}
+
+	public void setImageType(ImageType imageType) {
+		this.imageType = imageType;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
 	public SettingData() {
 		this.colorFilter = ColorFilter.none;
 		this.imageSize = ImageSize.any;
 		this.imageType = ImageType.any;
 	}
-	
-	public ImageSize getImageSize() {
-		return imageSize;
-	}
-	public ColorFilter getColorFilter() {
-		return colorFilter;
-	}
-	public ImageType getImageType() {
-		return imageType;
-	}
-	public String getSite() {
-		return site;
-	}
-
 }
--- src/com/example/imagesearch/settings/SettingsActivity.java
+++ src/com/example/imagesearch/settings/SettingsActivity.java
@@ -1,18 +1,19 @@
 package com.example.imagesearch.settings;
 
-import com.example.imagesearch.R;
-import com.example.imagesearch.R.layout;
-import com.example.imagesearch.R.menu;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.imagesearch.R;
 
 public class SettingsActivity extends Activity {
 	public static final String SettingDataKeyName = "sd";	
 	private SettingData data;
+	private Spinner imageSizeSpinner, colorFilterSpinner, imageTypeSpinner;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,24 @@ public class SettingsActivity extends Activity {
 		setContentView(R.layout.activity_settings);
 		
 		data = (SettingData) getIntent().getSerializableExtra(SettingDataKeyName);
+
+		imageSizeSpinner = (Spinner) findViewById(R.id.spinImageSize);
+		ArrayAdapter<ImageSize> imageSizeAdapter = new ArrayAdapter<ImageSize>(this, android.R.layout.simple_spinner_item, ImageSize.values());
+		imageSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		imageSizeSpinner.setAdapter(imageSizeAdapter);
+		imageSizeSpinner.setSelection(imageSizeAdapter.getPosition(data.getImageSize()));
+
+		colorFilterSpinner = (Spinner) findViewById(R.id.spinColorFilter);
+		ArrayAdapter<ColorFilter> colorFilterAdapter = new ArrayAdapter<ColorFilter>(this, android.R.layout.simple_spinner_item, ColorFilter.values());
+		colorFilterAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		colorFilterSpinner.setAdapter(colorFilterAdapter);
+		colorFilterSpinner.setSelection(colorFilterAdapter.getPosition(data.getColorFilter()));
+
+		imageTypeSpinner = (Spinner) findViewById(R.id.spinImageType);
+		ArrayAdapter<ImageType> imageTypeAdapter = new ArrayAdapter<ImageType>(this, android.R.layout.simple_spinner_item, ImageType.values());
+		imageTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		imageTypeSpinner.setAdapter(imageTypeAdapter);
+		imageTypeSpinner.setSelection(imageTypeAdapter.getPosition(data.getImageType()));
 	}
 	
 	@Override
@@ -30,6 +49,11 @@ public class SettingsActivity extends Activity {
 	
 	public void onSaveClick(MenuItem mi) {
 		Intent i = new Intent();
+		
+		data.setImageSize(ImageSize.valueOf(imageSizeSpinner.getSelectedItem().toString()));
+		data.setColorFilter(ColorFilter.valueOf(colorFilterSpinner.getSelectedItem().toString()));
+		data.setImageType(ImageType.valueOf(imageTypeSpinner.getSelectedItem().toString()));
+		
 		i.putExtra(SettingDataKeyName, data);
 		setResult(RESULT_OK, i);
 		finish();
